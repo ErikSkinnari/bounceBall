7cd6dbf97134e4d1fdc334d2844bc89b80912380
Erik Skinnari <erik@skinnari.se>
2021-05-20 13:47:28 +0200

    make cheating a little bit harder @Draws

--- minify.js
+++ minify.js
@@ -1,1 +1,265 @@
-let ball,HighScore,hs,highscorestable,PlayerName="Player",obstacles=[],paddleSpeed=10,paddleHits=0,speedchange=.4,Score=0;null!==window.localStorage.HighScore&&window.localStorage.HighScore>=0?HighScore=window.localStorage.HighScore:(window.localStorage.HighScore=0,HighScore=0);let canvasWidth=400;function preload(){hs=loadJSON("../gamehandler/gethighscores.php?gameid=1")}function setup(){setupGameBoard();let e=2,t=2;random(-1,1)<0&&(e*=-1),random(-1,1)<0&&(t*=-1),ball=new Ball(width/2,20,5,e,t),obstacles.push(new Paddle(20,width-40,60,10))}function draw(){background(0),stroke(255),ball.move(),ball.display(),0==ball.update()&&gameOver();for(let e=obstacles.length-1;e>=0;e--)obstacles[e].move(),obstacles[e].display();textSize(20),fill(255),Score>1e3&&noLoop(),textAlign(LEFT),text("Score: "+Score,20,30),textAlign(RIGHT),text("HighScore: "+HighScore,canvasWidth-20,30)}function setupGameBoard(){createCanvas(canvasWidth,canvasWidth).position(windowWidth/2-canvasWidth/2,200),textAlign(CENTER);let e=createDiv("Paddle Frenzy");e.class("title"),e.position(windowWidth/2-canvasWidth/2,50);let t=createDiv("<ul><li>Play with left and right arrows</li><li>The ball travels a little faster after every paddle touch</li><li>Now go get that highscore!</li><br><li>Press ENTER to reload</li></ul>");if(t.class("instructions"),t.position(100,200),null!==hs){let e=createDiv();e.class("Highscores"),e.position(windowWidth/2+canvasWidth/2+100,200);let t=createElement("h3","Highscores (WIP)");e.child(t);var i=createElement("table");let h=createElement("thead",'<tr><th style="text-align: left;">Player</th><th style="text-align: left;">Score</th></tr>');i.child(h);var s=createElement("tbody");for(var l in hs){let e=createElement("tr","<td>"+hs[l].playername+"</td><td>"+hs[l].score+"</td>");s.child(e)}i.child(s),e.child(i)}createElement("h3",PlayerName).position(windowWidth/2-canvasWidth/2,canvasWidth+210),createElement("form",'<input type="text" id="textbox" style="width: 100;" placeholder="Enter your Name"><button id="submit">Confirm Name</button>').position(windowWidth/2-canvasWidth/2,canvasWidth+260),document.getElementById("submit").addEventListener("click",(function(){window.localStorage.PlayerName=document.getElementById("textbox").value}))}function gameOver(){background(255,0,0),textAlign(CENTER),text("Game Over!",width/2,height/2-20),Score>HighScore&&text("New HighScore!",width/2,height/2+20),Score>HighScore&&(HighScore=Score,window.localStorage.HighScore=HighScore,h()),noLoop();let e=createButton("New Game");e.position(windowWidth/2-e.width/2,200+canvasWidth-100),e.mousePressed(e=>{location.reload()})}null!==window.localStorage.PlayerName&&(PlayerName=window.localStorage.PlayerName);class Ball{constructor(e,t,i,s,l){this.x=e,this.y=t,this.r=i,this.velX=s,this.velY=l,this.speedchange=speedchange}update(){if((this.velX>0&&this.x+this.r>=width||this.velX<0&&this.x-this.r<=0)&&(this.velX*=-1),this.velY<0&&this.y-this.r<=0&&(this.velY*=-1),this.velY>0&&this.y+this.r>=height)return!1;for(let e=obstacles.length-1;e>=0;e--)if(this.x+this.r+this.velX>obstacles[e].x&&this.x+this.velX<obstacles[e].x+obstacles[e].width&&this.y+this.r>obstacles[e].y&&this.y<obstacles[e].y+obstacles[e].height&&(this.velX*=-1),this.x+this.r>obstacles[e].x&&this.x<obstacles[e].x+obstacles[e].width&&this.y+this.r+this.velY>obstacles[e].y&&this.y+this.velY<obstacles[e].y+obstacles[e].height&&(this.velY*=-1,obstacles[e]instanceof Paddle)){Score+=10,paddleHits+=1,random(-1,1)<0?this.velX>0?this.velX+=this.speedchange:this.velX-=this.speedchange:this.velY>0?this.velY+=this.speedchange:this.velY-=this.speedchange}}move(){this.x+=this.velX,this.y+=this.velY}display(){ellipse(this.x,this.y,2*this.r,2*this.r)}}function keyTyped(){"r"===key&&(console.log("r"),window.localStorage.HighScore=0)}function keyPressed(){keyCode===ENTER&&location.reload()}function h(){fetch("../gamehandler/highscoreinsert.php?ga=1&is="+HighScore+"&sc="+Score+"&ph="+paddleHits+"&pn="+PlayerName,{method:"GET"}).then(e=>{console.log(e)}).catch(e=>console.log(JSON.parse(JSON.stringify(e))))}class Paddle{constructor(e,t,i,s){this.x=e,this.y=t,this.width=i,this.height=s}move(){keyIsDown(LEFT_ARROW)&&this.x>0?this.x-=paddleSpeed:keyIsDown(RIGHT_ARROW)&&this.x+this.width<width&&(this.x+=paddleSpeed)}display(){rect(this.x,this.y,this.width,this.height)}}
+// This code is very limited on comments on purpose. The variable names are not descriptive and badly named
+// The reason is to make it a weee bit more difficult to cheat a score into the highscore list. 
+
+// Name of the player - for the Highscore list
+let nm;
+if (window.localStorage.PlayerName !== null || window.localStorage.PlayerName !== "") {
+    nm = window.localStorage.PlayerName;
+} else {
+    nm = "Player";
+}
+
+let ta = 20;
+let ma = 120;
+const cs = 400;
+let ps = 10;
+let ph = 0;
+let sc = .4;
+let Score = 0;
+let hs;
+let hst;
+let f;
+const kw = 60;
+class U {
+    constructor(y, x, bs, hs) {
+        this.x = x;
+        this.y = y;
+        this.b = bs;
+        this.h = hs;
+    }
+
+    move() {
+        if ((keyIsDown(LEFT_ARROW) || keyIsDown(65)) && this.x > 0) {
+            this.x -= ps;
+        }
+        else if ((keyIsDown(RIGHT_ARROW) || keyIsDown(68)) && this.x + this.b < cs) {
+            this.x += ps;
+        }
+    }
+
+    display() {
+        rect(this.x, this.y, this.b, this.h);
+    }
+}
+
+const k = new U(cs - 20, (cs / 2 - 40), 60, 10);
+class Q {
+
+    constructor(x, y, r, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.sv = vx;
+        this.vv = vy;
+        this.sc = sc;
+    }
+
+    update() {
+
+        // Check for side collisions
+        if (this.sv > 0 && this.x + this.r >= cs || this.sv < 0 && this.x - this.r <= 0) {
+            this.sv *= -1;
+        }
+        // ceiling collision
+        if (this.vv < 0 && this.y - this.r <= 0) {
+            this.vv *= -1;
+        }
+        if (this.vv > 0 && this.y + this.r >= height) {
+            // Game Over
+            return false;
+        }
+
+        // Check for side collisions
+        if (this.x + this.r + this.sv > k.x &&
+            this.x + this.sv < k.x + k.b &&
+            this.y + this.r > k.y &&
+            this.y < k.y + k.h) {
+            this.sv *= -1;
+        }
+
+        // Check for top / bottom collisions
+        if (this.x + this.r > k.x &&
+            this.x < k.x + k.b &&
+            this.y + this.r + this.vv > k.y &&
+            this.y + this.vv < k.y + k.h) {
+            this.vv *= -1;
+
+            // Is this a paddle collision?
+            if (k instanceof U) {
+                Score += 10;
+                ph += 1;
+
+                // Increase ball speed, random on x OR y by 0,4
+                let xOrY = random(-1, 1)
+                if (xOrY < 0) {
+                    if (this.sv > 0) {
+                        this.sv += this.sc;
+                    }
+                    else {
+                        this.sv -= this.sc;
+                    }
+                }
+                else {
+                    if (this.vv > 0) {
+                        this.vv += this.sc;
+                    }
+                    else {
+                        this.vv -= this.sc;
+                    }
+                }
+            }
+        }
+    }
+
+    move() {
+        this.x += this.sv;
+        this.y += this.vv;
+    }
+
+    display() {
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
+}
+
+const z = new Q(cs / 2 / 2, 20, 5, (Math.random() < 0.5 ? -2 : 2), (Math.random() < 0.5 ? -2 : 2));
+
+
+if (window.localStorage.HighScore !== null && window.localStorage.HighScore >= 0) {
+    f = window.localStorage.HighScore;
+}
+else {
+    window.localStorage.HighScore = 0;
+    f = 0;
+}
+
+
+// Get current highscores
+function preload() {
+    let url = '../gamehandler/gethighscores.php?gameid=1';
+    hs = loadJSON(url);
+}
+
+function setupGameBoard() {
+    let canvas = createCanvas(cs, cs);
+    canvas.position(windowWidth / 2 - cs / 2, ma);
+
+    // Game title banner
+    textAlign(CENTER);
+    let pageTitle = createDiv('Paddle Frenzy');
+    pageTitle.class('title');
+    pageTitle.position(windowWidth / 2 - cs / 2, ta);
+
+    // Insructions (left side of gameplay canvas)
+    let howToPlay = createDiv(
+        '<ul><li>Play with left and right arrows or A & D</li>' +
+        '<li>The ball travels a little faster after every paddle touch</li>' +
+        '<li>Now go get that highscore!</li><br>' +
+        '<li>Press ENTER to reload</li></ul>');
+    howToPlay.class('instructions');
+    howToPlay.position(100, ma);
+
+    // If higscore list is loaded display the data
+    if (hs !== null) {
+        let Highscores = createDiv();
+        Highscores.class('Highscores');
+        Highscores.position(windowWidth / 2 + cs / 2 + 100, ma);
+        let hsTitle = createElement('h3', 'Highscores (WIP)');
+        Highscores.child(hsTitle);
+        var hsTable = createElement('table');
+        let hsTableHead = createElement('thead', '<tr><th style="text-align: left;">Player</th><th style="text-align: left;">Score</th></tr>');
+        hsTable.child(hsTableHead);
+        var hsTableBody = createElement('tbody');
+        for (var score in hs) {
+            let row = createElement('tr', '<td>' + hs[score].playername + '</td><td>' + hs[score].score + '</td>');
+            hsTableBody.child(row);
+        }
+        hsTable.child(hsTableBody);
+        Highscores.child(hsTable);
+    }
+
+    // Playername input field
+    let CurrentPlayer = createElement('h3', nm);
+    CurrentPlayer.position(windowWidth / 2 - cs / 2, ma + cs + 5);
+
+    let nameInput = createElement('form', '<input type="text" id="textbox" style="width: 100;" placeholder="Enter your Name"><button id="submit">Confirm Name</button>');
+    nameInput.position(windowWidth / 2 - cs / 2, ma + cs + 55);
+
+    document.getElementById('submit').addEventListener('click', function () {
+        window.localStorage.PlayerName = document.getElementById('textbox').value;
+    });
+}
+
+function setup() {
+    setupGameBoard();
+}
+
+function draw() {
+    background(0);
+    stroke(255);
+
+    z.move();
+    z.display();
+    if (z.update() == false) {
+        gameOver();
+    }
+
+    k.move();
+    k.display();
+    textSize(20);
+    fill(255);
+    if (Score > 1000) {
+        alert("No cheating!");
+    }
+    textAlign(LEFT);
+    text('Score: ' + Score, 20, 30);
+    textAlign(RIGHT);
+    text('HighScore: ' + f, cs - 20, 30);
+}
+
+function gameOver() {
+    background(255, 0, 0);
+    textAlign(CENTER);
+    text('Game Over!', cs / 2 / 2, height / 2 - 20);
+    if (Score > f) {
+        text('New HighScore!', cs / 2 / 2, height / 2 + 20);
+    }
+    if (Score > f) {
+        f = Score
+        window.localStorage.HighScore = f;
+        h();
+    }
+    noLoop();
+
+    let button = createButton('New Game');
+    button.position(windowWidth / 2 - button.width / 2, ma + cs - 100);
+    button.mousePressed(e => {
+        location.reload();
+    });
+}
+
+
+
+function keyTyped() {
+    if (key === 'r') {
+        console.log('r');
+        window.localStorage.HighScore = 0;
+    }
+}
+
+function keyPressed() {
+    if (keyCode === ENTER) {
+        location.reload();
+    }
+}
+
+function h() {
+    const y = k.bs
+    const url = '../gamehandler/highscoreinsert.php?ga=1&is=' + f + '&sc=' + Score + '&ph=' + ph + '&pn=' + nm + '&y=' + y;
+    const Param = {
+        method: "GET"
+    };
+    fetch(url, Param)
+        .then(res => { console.log(res) })
+        .catch(error => console.log(JSON.parse(JSON.stringify(error))))
+}
+
